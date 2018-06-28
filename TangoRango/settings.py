74c6d8cc00dce807b9820322d5b59fc505b9aeec
naduhrin78 <gokugohan123.com@gmail.com>
2018-06-28 10:17:27 +0530

    changed setting.py

--- TangoRango/settings.py
+++ TangoRango/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_PATH = os.path.join(BASE_DIR, 'template')
-STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
