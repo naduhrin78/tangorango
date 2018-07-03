509fb6c16ddffdc0824810417c65a53824704ed0
naduhrin78 <gokugohan123.com@gmail.com>
2018-07-03 18:44:34 +0530

    all

--- TangoRango/settings.py
+++ TangoRango/settings.py
@@ -93,15 +93,16 @@ WSGI_APPLICATION = 'TangoRango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-""""
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 """
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -112,7 +113,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-
 
 
 
