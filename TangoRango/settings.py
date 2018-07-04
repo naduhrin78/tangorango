45af4f8b23df7963962b1dea0088d124cbef3f70
N ANIRUDH <gokugohan123.com@gmail.com>
2018-07-04 15:57:14 +0530

    Update settings.py

--- TangoRango/settings.py
+++ TangoRango/settings.py
@@ -154,10 +154,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-
+        STATIC_ROOT,
     ]
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-
